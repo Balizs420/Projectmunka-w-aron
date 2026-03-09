242a10bf93f2871d03d17d26f66b4098c386663f
gruberaron25-commits <gruberaron25@gmail.com>
2026-03-09 20:49:01 +0100

    Javítás

---
+++ script.js
@@ -0,0 +1,8 @@
+function myfunction() {
+    document.getElementById('bekezdes').innerHTML = x + " " + y;
+    document.getElementById('kep').style.display = "block";
+    window.alert("Várai Balázs")
+    
+}
+let x = "Gruber";
+let y = "Lili";
