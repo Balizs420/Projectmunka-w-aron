00a162c10dbf11f720345f65ceec247f95752394
gruberaron25-commits <gruberaron25@gmail.com>
2026-03-13 19:23:30 +0100

    jaja

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ function myfunction() {
     let kepecske = document.getElementById("kep");
     kepecske.classList.toggle("fordit")
 }
+
+let toggled2 = false;
+
+function myfunction2() {
+
+    let div2 = document.getElementById('doboz2');
+    div2.classList.toggle('nyitva2')
+
+    let kepecske2 = document.getElementById("kep2");
+    kepecske2.classList.toggle("fordit2")
+
+}
+
